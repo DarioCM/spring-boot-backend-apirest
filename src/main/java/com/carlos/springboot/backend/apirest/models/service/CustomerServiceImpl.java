860db9d2af9101f656aaa332becc4945d6031e65
Carlos D <dario20049@icloud.com>
2024-03-28 22:42:38 +0900

    Service Impl

---
+++ src/main/java/com/carlos/springboot/backend/apirest/models/service/CustomerServiceImpl.java
@@ -0,0 +1,22 @@
+package com.carlos.springboot.backend.apirest.models.service;
+
+import com.carlos.springboot.backend.apirest.models.dao.ICustomerDao;
+import com.carlos.springboot.backend.apirest.models.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements ICustomerService {
+
+    @Autowired
+    private ICustomerDao customerDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Customer> findAll() {
+        return (List<Customer>) customerDao.findAll();
+    }
+}
---
+++ src/main/java/com/carlos/springboot/backend/apirest/models/service/ICustomerService.java
@@ -0,0 +1,11 @@
+package com.carlos.springboot.backend.apirest.models.service;
+
+import com.carlos.springboot.backend.apirest.models.entity.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+
+    public List<Customer> findAll();
+
+}
