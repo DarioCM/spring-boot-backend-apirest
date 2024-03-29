c27b3a160188176156c6dd207679b8558fbbb4e1
Carlos D <dario20049@icloud.com>
2024-03-29 20:32:05 +0900

    add CROSS origins

--- src/main/java/com/carlos/springboot/backend/apirest/controller/CustomerRestController.java
+++ src/main/java/com/carlos/springboot/backend/apirest/controller/CustomerRestController.java
@@ -3,12 +3,14 @@ package com.carlos.springboot.backend.apirest.controller;
 import com.carlos.springboot.backend.apirest.models.entity.Customer;
 import com.carlos.springboot.backend.apirest.models.service.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController //API rest
 @RequestMapping("/api") //url enpoint
 public class CustomerRestController  {
