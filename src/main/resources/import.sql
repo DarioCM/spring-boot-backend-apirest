2da01ff38d16d79d74eb66126f95971ccf7cf1a2
Carlos D <dario20049@icloud.com>
2024-03-29 20:21:58 +0900

    Populate Customers table

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+/* Populate Customers table */
+INSERT INTO customer (name, last_name, email, create_at) VALUES ('TEST','TEST','email@testing.com','2024-01-01');
+INSERT INTO customer (name, last_name, email, create_at) VALUES ('TEST','TEST','email@testing.com','2024-01-01');
+INSERT INTO customer (name, last_name, email, create_at) VALUES ('TEST','TEST','email@testing.com','2024-01-01');
+INSERT INTO customer (name, last_name, email, create_at) VALUES ('TEST','TEST','email@testing.com','2024-01-01');
+INSERT INTO customer (name, last_name, email, create_at) VALUES ('TEST','TEST','email@testing.com','2024-01-01');
+INSERT INTO customer (name, last_name, email, create_at) VALUES ('TEST','TEST','email@testing.com','2024-01-01');
+INSERT INTO customer (name, last_name, email, create_at) VALUES ('TEST','TEST','email@testing.com','2024-01-01');
+INSERT INTO customer (name, last_name, email, create_at) VALUES ('TEST','TEST','email@testing.com','2024-01-01');
+INSERT INTO customer (name, last_name, email, create_at) VALUES ('TEST','TEST','email@testing.com','2024-01-01');
